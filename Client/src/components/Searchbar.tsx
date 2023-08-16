97ec965f994accbaae0a2b98183d6e3612714734
Eros <elewites@gmail.com>
2023-08-16 16:01:56 -0700

    utilize searchTerm global state in Searchbar.tsx

--- Client/src/components/Searchbar.tsx
+++ Client/src/components/Searchbar.tsx
@@ -1,17 +1,25 @@
 // css styles import
-import "./css/searchbar.css"
+import "./css/searchbar.css";
 
 // libraries import 
-import React from 'react';
+import { useState, ChangeEvent, useContext } from 'react';
+import react from 'react';
+import { SearchTermContext } from "../contexts/searchTermContext";
+
 
 export interface searchBarPropsInterface {
-  
+
 }
 
 function SearchBar(props: searchBarPropsInterface) {
+
+  const { searchTerm, setSearchTerm } = useContext(SearchTermContext);
+
   return (
     <div className="searchbar-container">
-      <input type="text" placeholder="Search..." />
+      <input type="text" placeholder="Search..." onChange={(event) => {
+        setSearchTerm(event.target.value);
+      }} />
       <button>Search</button>
     </div>
   );
