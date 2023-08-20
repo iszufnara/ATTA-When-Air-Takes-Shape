99a64f531b74cea82ce41855055a3e37e4a7cc40
Eros <elewites@gmail.com>
2023-08-20 00:24:21 -0700

    modify props type in MapRoute and Map

--- Client/src/routes/MapRoute.tsx
+++ Client/src/routes/MapRoute.tsx
@@ -1,36 +1,35 @@
 // import statements 
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useContext } from 'react';
 import { DataHandler } from "../model/DataHandler";
 import { useMemo } from 'react';
 import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
+import { citiesCountriesPropsInterface } from '../model/interfaces';
+import { SearchInfoContext } from '../contexts/searchInfoContext';
 import "./css/map.css";
 
 // google maps API key
 const key = "AIzaSyCrqndBV56Fm8dTXCtAIPOyqYkqXQEDbKA";
 
-// Declaring type of props
-export interface mapPropsInterface {
-}
 
 // MapRoute that is rendered in App.tsx
-export function MapRoute() {
+export const MapRoute: React.FC<citiesCountriesPropsInterface> = ({ cities, countries, setCities, setCountries }) => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: key
   });
   if (!isLoaded) return <div>Loading...</div>;
-  return <Map />;
-}
+  return <Map {...{ cities, countries, setCities, setCountries }} />;
+};
 
 // renders GoogleMap and Markers
-function Map() {
+function Map({ cities, countries, setCities, setCountries }: citiesCountriesPropsInterface) {
   const center = useMemo(() => ({ lat: 49.282730, lng: -123.120735 }), []);
-  const dataHandler = new DataHandler();
+  const { searchInfo, setSearchInfo } = useContext(SearchInfoContext);
   return (
-    <>
+    <div>
       <GoogleMap zoom={8} center={center} mapContainerClassName="map-route-container">
         <MarkerF position={center} />
-        {dataHandler.data.map((datapoint) => <MarkerF position={{ lat: datapoint.lat, lng: datapoint.lon }} />)}
+        {/* {dataHandler.data.map((datapoint) => <MarkerF position={{ lat: datapoint.lat, lng: datapoint.lon }} />)} */}
       </GoogleMap>
-    </>
+    </div>
   );
 };
