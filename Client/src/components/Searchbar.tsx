eb45b6d1fb3dfe3e310f08103fda414e8fdf2e50
Eros <elewites@gmail.com>
2023-08-20 00:23:56 -0700

    add filter functionality to SearchBar.tsx, modify props type

--- Client/src/components/Searchbar.tsx
+++ Client/src/components/Searchbar.tsx
@@ -4,23 +4,41 @@ import "./css/searchbar.css";
 // libraries import 
 import { useState, ChangeEvent, useContext } from 'react';
 import react from 'react';
-import { SearchTermContext } from "../contexts/searchTermContext";
+import { SearchInfoContext } from "../contexts/searchInfoContext";
+import { citiesCountriesPropsInterface } from "../model/interfaces";
+import { City, Country } from "../model/DataHandler";
 
+function SearchBar({ cities, countries }: citiesCountriesPropsInterface) {
+  const { searchInfo, setSearchInfo } = useContext(SearchInfoContext);
 
-export interface searchBarPropsInterface {
+  /**
+   * filterEmpty is an array of Country or City that has been filtered to exclude empty strings 
+   * in the props states (countries or cities)
+   */
+  const filterEmpty: Array<Country | City> = countries.filter((city: City | Country) => {
+    return city != "";
+  });
 
-}
-
-function SearchBar(props: searchBarPropsInterface) {
-
-  const { searchTerm, setSearchTerm } = useContext(SearchTermContext);
+  /**
+   * searchFiltered is an array of Country or City that is dynamically filered as searchInfo.term state changes. 
+   * searchInfo.term changes according to user input. 
+   */
+  const searchFiltered = filterEmpty.filter((city) =>
+    city?.toLowerCase().includes(searchInfo.term.toLowerCase())
+  );
 
   return (
     <div className="searchbar-container">
       <input type="text" placeholder="Search..." onChange={(event) => {
-        setSearchTerm(event.target.value);
+        setSearchInfo({ ...searchInfo, term: event.target.value });
       }} />
+      <button onClick={() => setSearchInfo({ ...searchInfo, byCity: false })}>by country</button>
+      <button onClick={() => setSearchInfo({ ...searchInfo, byCity: true })}>by city</button>
       <button>Search</button>
+      {searchFiltered.map((city, key) => {
+        return (<text>{city}...</text>);
+      })}
+      <text>{cities.length}</text>
     </div>
   );
 }
