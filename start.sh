8fb45e812adf514985cce7eb0f870f5f745ba244
TobiasB4 <Tobi.Bobb.TB@gmail.com>
2023-08-27 12:30:52 -0700

    Typo Fix

--- start.sh
+++ start.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-TASKILL //F //IM node.exe
+TASKKILL //F //IM node.exe
 
-projectPath="/Users/WATS/Documents/ATTA-When-Air-Takes-Shape"
-serverPath="$projectPath/Server"
-clientPath="$projectPath/Client"
-
-cd "$projectPath" || exit 1
 git checkout staging-branch
 git pull
 
-cd "$serverPath" || exit 1
+cd "Server" || exit 1
 npm install
 npm install -g ts-node
 ts-node server.ts &
 
-cd "$clientPath" || exit 1
+cd "../Client" || exit 1
 npm install
 npm start &
